7617e1153e779a7530000bf7283f0e1c0a5e08fe
Jonathan Felix <123984244+JonaThFelix@users.noreply.github.com>
2023-05-16 00:18:20 -0300

    Update oDate.py

--- oDate.py
+++ oDate.py
@@ -2,11 +2,23 @@ from tkinter import  *
 import tkinter as tk
 import tkinter.messagebox
 import random
+import customtkinter
 
 # layout geral da aplicação
-root = tk.Tk()
-root.geometry('300x300') 
+root = customtkinter.CTk()
 root.title('Em busca da Pequena')
+largura = 400
+altura = 400
+
+largura_tela = root.winfo_screenwidth()
+altura_tela = root.winfo_screenheight()
+
+posX = largura_tela / 2 - largura / 2
+posY = altura_tela / 2 - altura  / 2
+
+root.geometry("%dx%d+%d+%d"%(largura, altura, posX, posY))
+root._set_appearance_mode('System')
+root.resizable(False,False)
 root.configure(background='pink')
 
 def houver(event):
@@ -15,21 +27,21 @@ def houver(event):
     nao.place(x=x,y=y)
 
 def onClick(): 
-    tk.messagebox.showinfo("John Diz...",  "Ótimo, Agora é comigo !!!")
-    
+    tk.messagebox.showinfo(" Diz...",  "Ótimo, o resto eu resolvo !!!")
+
 
 # título
-pergunta = tk.Label(root, text="Quando vamos sair?")
+pergunta = customtkinter.CTkLabel(root, text="Quando vamos Sair?")
 pergunta.pack(anchor='n', padx=20)
-pergunta.place(x=80,y=20)
+pergunta.place(x=130,y=40)
 
 
 # configuração dos botões
-nao = tk.Button(root, text="Não Sei",bg="red",fg="yellow")
+nao = customtkinter.CTkButton(root, text="Não Sei",text_color='yellow',fg_color="red")
 nao.place(x=180, y=100)
 nao.bind("<Enter>", houver)
 
-sim = tk.Button(root, text="Quando vc quiser",command=onClick)
+sim = customtkinter.CTkButton(root, text="Quando vc quiser",fg_color='pink',text_color='black', hover_color='#DC143C',command=onClick)
 sim.place(x=50, y=100) 
 
 root.mainloop()
